29f94c2b942ffd09f5734e3fddcd4d5583438310
storres8 <stevetorres7562@gmail.com>
2019-02-22 23:44:44 -0500

    added css stylings and state.

--- contactmanager/src/Components/Contact.js
+++ contactmanager/src/Components/Contact.js
@@ -1,24 +1,50 @@
 import React, { Component } from 'react';
+import "../Assets/Contact.css"
 
 class Contact extends Component {
+    
+    state={
+        dropDown: false 
+    }
+
+
+    handleDropDown = (e) =>{
+        this.setState({dropDown: !this.state.dropDown})
+    }
+
   render() {
     const { name, email, phone, id}= this.props
 
     const cardStyle={
-        width: '18rem'
+        width: '28rem'
+    }  
+
+    const deleteStyle={
+        float:"right"
     }
 
     return (
-        <div>
-            <div className="container row justify-content-center">
+        <div className="container">
+            <div className="container row justify-content-center" id="cards">
                 <div className="card" style={cardStyle} >
-                    <div className="card-header">
-                        {name}
+                    <div className="card-header">   
+                        <h1 id="contactName">
+                            {name}
+                            <i 
+                                className="fas fa-caret-down" id="dropDown"
+                                onClick = {this.handleDropDown}
+                            ></i>
+                            
+                        </h1>
+                        <div style={deleteStyle}><i className="fas fa-trash-alt"></i></div>
                     </div>
-                    <ul className="list-group list-group-flush">
-                        <li className ="list-group-item">{email}</li>
-                        <li className="list-group-item">{phone}</li>
-                    </ul> 
+                    {this.state.dropDown ? 
+                        (<ul className="list-group list-group-flush">
+                            <li className ="list-group-item"><h5>{email}</h5></li>
+                            <li className="list-group-item"><h5>{phone}</h5></li>
+                        </ul>) 
+                            : null
+                    }
                 </div>
             </div>
         </div>
